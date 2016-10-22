20f4efe46be631bfc2c02718ca8175118a84b9ae
Léna Faure <lena.faure@gmail.com>
2016-10-23 00:01:28 +0200

    Last modifs for today

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function(){
 
     // Fetching the elements with className "image"
     var image = document.getElementsByClassName("image");
-
+    var preview = document.getElementById("preview");
 
     // Writing function for opening large Image
     var openLargeImage = function() {
@@ -10,9 +10,12 @@ window.onload = function(){
      // On the clicked element, adding a class "open" wich we defined in the CSS and creates the scale              transition
      this.className += " open";
 
+     preview.className = "";
+        preview.className = "show";
+
      // Take care of the fact that the fixed element we get isn't vertically centered
 
-        
+
      var windowHeight = window.innerHeight;
      var contentHeight = this.offsetHeight;
      this.style.top = ((windowHeight / 2) - (contentHeight / 2)) + 'px';
