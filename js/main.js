81f6c571c2d3a659d286f3cd1c58730c4b60de7a
Léna Faure <lena.faure@gmail.com>
2016-10-22 22:46:15 +0200

    Vertically center scaled image and comment Javascript

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 window.onload = function(){
 
+    // Fetching the elements with className "image"
     var classname = document.getElementsByClassName("image");
 
-    var openLargeImage = function() {
-
-
-       this.className += " open";
-
-      /*  setTimeout(function(){
-            document.getElementById("content").className += "hide";
-        }, 1000); */
-
-
-
-
-    };
-
+    // Add an eventListener on this element
     for (var i = 0; i < classname.length; i++) {
         classname[i].addEventListener('click', openLargeImage, false);
     }
 
+    // Writing function for opening large Image
+    var openLargeImage = function() {
+
+     // On the clicked element, adding a class "open" wich we defined in the CSS and creates the scale              transition
+     this.className += " open";
+
+     // Take care of the fact that the fixed element we get isn't vertically centered
+     var windowHeight = window.innerHeight;
+     var contentHeight = this.offsetHeight;
+     this.style.top = ((windowHeight / 2) - (contentHeight / 2)) + 'px';
+
+    };
+    
 };
