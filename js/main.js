69ad1e757da051f09577b47416a5cd62192c06b2
Léna Faure <lena.faure@gmail.com>
2016-10-23 17:26:43 +0200

    Thumbnail to large image

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onload = function(){
 
     // Fetching the elements with className "image"
     var image = document.getElementsByClassName("image");
+    var thumbnail = document.getElementsByClassName("thumbnail");
     var preview = document.getElementById("preview");
     var close = document.getElementById("close");
 
@@ -21,6 +22,36 @@ window.onload = function(){
 
     };
 
+    // Writing function for opening large Image
+    var openThumbnailImage = function() {
+
+        // Use src of clicked thumbnail to find corresponding large sized image
+        imagePath = this.src;
+
+        // Loop through large sized images
+        for (var i = 0; i < image.length; i++) {
+
+            // When we find the corresponding image
+            if(imagePath == image[i].src) {
+
+                // Identify it by giving a transitory id
+                image[i].id = "waitingforclick";
+
+
+                // Close the preview
+                closePreview();
+
+                // Simulate a click on the large size image, which opens a new preview with the image corresponding to the clicked thumbnail
+                document.getElementById('waitingforclick').click();
+
+                // Suppress transitory id
+                document.getElementById('waitingforclick').id=  "";
+            }
+
+        }
+
+    };
+
     // Writing function for closing overlay and large size image
     var closePreview = function() {
 
@@ -29,7 +60,7 @@ window.onload = function(){
             image[i].className = "image";
             image[i].style.top = "";
         }
-
+        
         // Add a classname "hide" to hide overlay
         preview.className = "hide";
 
@@ -38,6 +69,11 @@ window.onload = function(){
     // Add an eventListener on the "image" element
     for (var i = 0; i < image.length; i++) {
         image[i].addEventListener('click', openLargeImage, false);
+    }
+
+    // Add an eventListener on the "thumbnail" element
+    for (var i = 0; i < thumbnail.length; i++) {
+        thumbnail[i].addEventListener('click', openThumbnailImage, false);
     }
 
     // Add an eventListener on the "close" element
